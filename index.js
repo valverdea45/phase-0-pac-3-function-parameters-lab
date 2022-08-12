643230346e1c83a976fdd78f7632ebacd0d0741d
Adrian <adrianvalverde45@gmail.com>
2022-08-12 15:48:41 -0400

    commit tree 4f0606b436d9d74fe12a993b5f433081efdebe57

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name){
+    return (`Hi, my name is ${name}.`)
+}
+
+introduction()
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+};
+
+introductionWithLanguage();
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+};
+
+introductionWithLanguageOptional();
